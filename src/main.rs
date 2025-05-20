a635a9705794660bbc76ebafb1bcee1fb0c028cd
dotun <dkolaolaleye@meetsoci.com>
2025-05-20 02:39:17 -0600

    Return echo

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,40 @@
+use regex::Regex;
 use std::io::{Read, Write};
-use std::net::{TcpListener,  TcpStream};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-    
+
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
                 let data = read_data(&mut _stream);
-                
+
                 let (request_line, _) = data.split_once("\r\n").unwrap();
                 let request_path = parse_header(request_line).path;
-                
+
                 let response: &str;
-                
+                let formatted;
+
                 match request_path.as_str() {
                     "/" => {
                         response = "HTTP/1.1 200 OK\r\n\r\n";
-                    },
+                    }
+                    s if {
+                        let pattern = Regex::new(r"/echo/.+").unwrap();
+                        pattern.is_match(s.trim())
+                    } =>
+                        {
+                            let path = request_path.split_once("/echo/").unwrap().1;
+                            formatted = format!("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", path.len(), path);
+                            response = formatted.trim();
+                        }
                     _ => {
                         response = "HTTP/1.1 404 Not Found\r\n\r\n";
                     }
                 }
                 println!("Request was {} and response was {}", data, response);
                 _stream.write_all(response.as_bytes()).unwrap();
-                
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -35,9 +45,9 @@ fn main() {
 
 fn read_data(stream: &mut TcpStream) -> String {
     let mut buffer = [0; 1024];
-    
+
     stream.read(&mut buffer).unwrap();
-    
+
     String::from_utf8_lossy(&buffer[..]).trim().to_string()
 }
 
