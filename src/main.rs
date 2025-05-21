559fd3c384f6de06918aedaf8fccede5efdd4089
dotun <dkolaolaleye@meetsoci.com>
2025-05-21 01:17:54 -0600

    Setting file length

--- src/main.rs
+++ src/main.rs
@@ -123,10 +123,12 @@ fn save_content_to_file_path(
     let file_path = format!("{directory}{file_name}");
 
     let mut file = fs::File::create(file_path)?;
-    file.set_len(55)?;
 
     match file.write(content.trim().as_bytes()) {
-        Ok(_) => Ok(()),
+        Ok(_) => {
+            file.set_len(55)?;
+            Ok(())
+        }
         Err(_) => panic!("Couldn't save file"),
     }
 }
